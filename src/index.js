603fd7a4137ece5f5d868f4aa6aaaf245c4f671b
Pavlo Serhiichuk <pavel.serhiichuk@gmail.com>
2025-07-17 19:53:59 +0300

    webpack - added loaders - css, png, etc.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import _ from 'lodash';
+import './style.css';
+import ImageItem from './image.png';
+
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello')
 
+  // Add image to bg
+  // element.style.backgroundImage = `url(${Image})`
+  // element.style.backgroundSize = `350px`
+  // element.style.backgroundRepeat = `no-repeat`
+  // element.style.objectFit = 'cover'
+  // element.style.height = '100vh'
+
+  // Add the image to our existing div.
+  const myImage = new Image();
+  myImage.src = ImageItem;
+  element.appendChild(myImage)
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,23 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: "main.[contenthash:8].js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, './dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
