dbcd6a8e823625dedf2a6e03777c1ff86f38471d
Pavlo Serhiichuk <pavel.serhiichuk@gmail.com>
2025-07-18 13:55:29 +0300

    delete redundant

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import _ from 'lodash';
 import './style.css';
-import ImageItem from './image.png';
 
 function component() {
   const element = document.createElement('div');
@@ -9,17 +8,6 @@ function component() {
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello')
 
-  // Add image to bg
-  // element.style.backgroundImage = `url(${Image})`
-  // element.style.backgroundSize = `350px`
-  // element.style.backgroundRepeat = `no-repeat`
-  // element.style.objectFit = 'cover'
-  // element.style.height = '100vh'
-
-  // Add the image to our existing div.
-  const myImage = new Image();
-  myImage.src = ImageItem;
-  element.appendChild(myImage)
   return element;
 }
 
