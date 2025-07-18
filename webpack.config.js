ace858b3dd4f1ab0760d011960a28ff03f69ce6a
Pavlo Serhiichuk <pavel.serhiichuk@gmail.com>
2025-07-18 17:11:26 +0300

    Development - https://webpack.js.org/guides/development/

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,46 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    index: './src/index.js',
-    print: './src/print.js',
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, './dist'),
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Output Management!',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
+module.exports = () =>  {
+  return {
+    mode: 'development',
+    entry: {
+      index: './src/index.js',
+      print: './src/print.js',
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, './dist'),
+      clean: true,
+      publicPath: '/',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Development',
+      }),
     ],
-  },
+    devtool: 'inline-source-map',
+    devServer: {
+      static: './dist',
+    },
+    optimization: {
+      runtimeChunk: 'single',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+  }
 };
