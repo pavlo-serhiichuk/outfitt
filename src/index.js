3b3c65ab65f76f876700571aaca22da144bc98f5
Pavlo Serhiichuk <pavel.serhiichuk@gmail.com>
2025-07-19 13:57:37 +0300

    Config plugins: HtmlWebpackPlugin, MiniCssExtractPlugin Add loaders: sass-loader, sass Add scripts: 'build:prod', 'build:dev'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import './style.css';
+import './style.scss';
 
 function component() {
   const element = document.createElement('div');
@@ -8,8 +8,8 @@ function component() {
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello')
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
+  btn.innerHTML = 'Click me and check the console! ~!!!!!!~~!';
+  btn.onclick = () => console.log('printed');
 
   element.appendChild(btn);
   return element;
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,39 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = () =>  {
+module.exports = (env) => {
+  // console.log('mode: ', env.mode)
   return {
     mode: 'development',
     entry: {
       index: './src/index.js',
-      print: './src/print.js',
     },
     output: {
-      filename: '[name].bundle.js',
+      filename: 'main.[contenthash:10].js',
       path: path.resolve(__dirname, './dist'),
       clean: true,
-      publicPath: '/',
     },
     plugins: [
       new HtmlWebpackPlugin({
-        title: 'Development',
+        template: path.resolve(__dirname, 'public', 'index.html'),
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'main.[contenthash:10].css', // Output filename for the extracted CSS
       }),
     ],
     devtool: 'inline-source-map',
     devServer: {
-      static: './dist',
-    },
-    optimization: {
-      runtimeChunk: 'single',
+      hot: true,
+      port: 3010,
+      open: true,
+      historyApiFallback: true,
     },
     module: {
       rules: [
         {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          test: /\.s[ac]ss$/i,
+          use: [env.mode === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
         },
         {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -39,6 +42,9 @@ module.exports = () =>  {
         {
           test: /\.(woff|woff2|eot|ttf|otf)$/i,
           type: 'asset/resource',
+          generator: {
+            filename: 'fonts/[name][ext]', // <-- this moves them to "dist/fonts/"
+          },
         },
       ],
     },
