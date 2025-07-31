359a06221f5aff9fabad0fb3b582da236e099c4d
Pavlo Serhiichuk <pavel.serhiichuk@gmail.com>
2025-07-31 14:53:33 +0300

    added TestAsyncThunk.ts class

---
+++ src/shared/lib/jest/TestAsyncThunk/TestAsyncThunk.ts
@@ -0,0 +1,23 @@
+import {StateSchema} from "@/app/providers/StoreProvider/StateSchema";
+import {AsyncThunkAction} from "@reduxjs/toolkit";
+
+type ActionCreatorType<Returned, Arg, RejectValue> = (arg: Arg) => AsyncThunkAction<Returned, Arg, RejectValue>
+
+export class TestAsyncThunk<Returned, Arg, RejectValue> {
+  dispatch: jest.MockedFn<any>
+  getState: () => StateSchema
+  actionCreator: ActionCreatorType<Returned, Arg, RejectValue>
+
+  constructor(actionCreator: ActionCreatorType<Returned, Arg, RejectValue>) {
+    this.actionCreator = actionCreator;
+    this.dispatch = jest.fn();
+    this.getState = jest.fn();
+  }
+
+  async callThunk(arg: Arg) {
+    const action = this.actionCreator(arg)
+    // @ts-ignore
+    const result =  await action(this.dispatch, this.getState, undefined)
+    return result
+  }
+}
